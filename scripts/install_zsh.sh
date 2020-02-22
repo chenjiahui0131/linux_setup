94a800a07b0511878810026c2f8ad592ca6359f2
Coffee <chenjiahui1991@163.com>
2020-02-22 17:16:41 +0000

    fix install_zsh.sh

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -3,12 +3,12 @@ set -ex
 INSTALL_LOG_DIR="$HOME/.install-log"
 mkdir -p $INSTALL_LOG_DIR
 ZSH_DONE="$INSTALL_LOG_DIR/zsh.done"
-if [ ! -e "$TMUX_DONE" ]; then
+if [ ! -e "$ZSH_DONE" ]; then
 	BASE_DIR=`pwd`
 	apt-get install -y zsh
 	sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 	chsh -s /usr/bin/zsh $USER
 	sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
-	touch ZSH_DONE
+	touch $ZSH_DONE
 fi
 
