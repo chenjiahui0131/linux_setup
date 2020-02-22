fac11721a1765301aad28b1c5cccc20652f25fff
Coffee <chenjiahui1991@163.com>
2020-02-22 16:58:00 +0000

    fix install_tmux.sh

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -4,15 +4,15 @@ INSTALL_LOG_DIR="$HOME/.install-log"
 mkdir -p $INSTALL_LOG_DIR
 TMUX_DONE="$INSTALL_LOG_DIR/tmux.done"
 if [ ! -e "$TMUX_DONE" ]; then
-	PWD=`pwd`
+	BASE_DIR=`pwd`
 	cd /tmp/
 	wget -N https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
 	tar -zxf tmux-*.tar.gz
 	cd tmux-3.0a/
 	./configure
 	make && make install
-	cd $PWD
+	cd $BASE_DIR
 	rm -f ~/.tmux.conf
-	ln -s `pwd`/../config/tmux/.tmux.conf ~/.tmux.conf
+	ln -s $BASE_DIR/../config/tmux/.tmux.conf ~/.tmux.conf
 	touch $TMUX_DONE
 fi
